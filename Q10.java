acfb4fecb67982d27b8a5c1c64b4f3a43bff2cd4
bagtyhnz <80709460+bagtyhnz@users.noreply.github.com>
2021-03-20 01:43:12 +0100

    Add files via upload

---
+++ Q10.java
@@ -0,0 +1,15 @@
+package assignments.variables;
+
+public class Q10 {
+
+	public static void main(String[] args) {
+
+
+		int hour=12, minute=36, second=53;
+		String am="AM", pm="PM";
+		
+		System.out.println(hour + ":" + minute + ":"+ second + " "+am );
+
+	}
+
+}
